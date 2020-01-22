1a7494a09cd490105317e89b8f9258a04fba00d0
Juan C <juanceballos@pursuit.org>
2020-01-22 09:51:36 -0500

    "Added Network Helper"

---
+++ Dependencies-Lab/Dependencies-Lab/Network Services/PhotoAPI.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoAPI.swift
+//  Dependencies-Lab
+//
+//  Created by Juan Ceballos on 1/22/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+class PhotoAPI  {
+    static func fetchPhotos()   {
+        
+    }
+}
